a24f74a23026abdde91828d0f7b92304dbd0a850
Joseph King <jgkingii@gmail.com>
2020-04-04 07:02:04 -0400

    Boom...issues gallore.

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
